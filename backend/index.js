caa43d5f7da352a01f1484db6364aa6b070a8de1
Aman Kumar <ar95046@gmail.com>
2026-01-14 16:00:39 +0530

    connect backend to db

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { createTodo, updateTodo } = require("./types");
+const { default: mongoose } = require("mongoose");
+const { todo } = require("./db");
 const app = express();
 
 app.use(express.json());
 
-app.post("/todo", (req, res) => {
+app.post("/todo", async (req, res) => {
   const title = req.body.title;
   const description = req.body.description;
 
@@ -18,14 +20,24 @@ app.post("/todo", (req, res) => {
   }
 
   //after validation put the content in mongodb
-  return res.json({
+  await todo.create({
+    title: title,
+    description: description,
+    completed: false,
+  });
+  res.json({
     msg: "Todo Created Successfully",
   });
 });
 
-app.get("/todos", (req, res) => {});
+app.get("/todos", async (req, res) => {
+  const todos = await todo.find({});
+  res.json({
+    todos: todos,
+  });
+});
 
-app.put("/completed", (req, res) => {
+app.put("/completed", async (req, res) => {
   const id = req.body.id;
 
   //validating id
@@ -39,6 +51,18 @@ app.put("/completed", (req, res) => {
   }
 
   //after validation update the content in mongodb
+  await todo.update(
+    {
+      _id: id,
+    },
+    {
+      completed: true,
+    }
+  );
+
+  res.send({
+    msg: "Todo mark as completed",
+  });
 });
 
 app.listen(3000);
